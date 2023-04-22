9b5187a224921681c7e25d28dce049732c3b40d8
lild4d4 <daniel.arevalos@sansano.usm.cl>
2023-04-22 03:20:07 +0200

    design_2ro

--- src/USM_ringoscillator.v
+++ src/USM_ringoscillator.v
@@ -10,8 +10,8 @@ module nand_with_delay(
 //assign #0.02 Y=~A;
 assign #200 Y=~A;
 `else
-//sky130_fd_sc_hd__nand4_4 nand4(.Y(Y), .A(A), .B(A), .C(A), .D(A));
-nor #(2) (Y,A,A);
+sky130_fd_sc_hd__nand4_4 nand4(.Y(Y), .A(A), .B(A), .C(A), .D(A));
+//nor #(2) (Y,A,A);
 `endif
 endmodule
 
@@ -24,8 +24,8 @@ module inv_with_delay(
 //assign #0.02 Y=~A;
 assign #200 Y=~A;
 `else
-//sky130_fd_sc_hd__inv_2 inv(.A(A),.Y(Y));
-nor #(2) (Y,A,A);
+sky130_fd_sc_hd__inv_2 inv(.A(A),.Y(Y));
+//nor #(2) (Y,A,A);
 `endif
 endmodule
 
