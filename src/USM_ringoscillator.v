8bb41e69b1beba12494389a217dd707c8b87bce1
lild4d4 <daniel.arevalos@sansano.usm.cl>
2023-04-22 02:57:01 +0200

    test_design_with_2ro

--- src/USM_ringoscillator.v
+++ src/USM_ringoscillator.v
@@ -1,6 +1,20 @@
 `default_nettype none
 `timescale 1ns/1ps
 
+module nand_with_delay(
+	input A, 
+	output Y
+);
+
+`ifdef COCOTB_SIM
+//assign #0.02 Y=~A;
+assign #200 Y=~A;
+`else
+//sky130_fd_sc_hd__nand4_4 nand4(.Y(Y), .A(A), .B(A), .C(A), .D(A));
+nor #(2) (Y,A,A);
+`endif
+endmodule
+
 module inv_with_delay(
 	input A, 
 	output Y
@@ -11,12 +25,36 @@ module inv_with_delay(
 assign #200 Y=~A;
 `else
 //sky130_fd_sc_hd__inv_2 inv(.A(A),.Y(Y));
-sky130_fd_sc_hd__nand4_4 nand4(.Y(Y), .A(A), .B(A), .C(A), .D(A));
-//nor #(2) (Y,A,A);
+nor #(2) (Y,A,A);
 `endif
 endmodule
 
-module USM_ringoscillator(
+module USM_ringoscillator_inv2(
+
+	input en,
+	output out
+
+);
+
+
+localparam etapas = 99;
+
+wire aux_wire [etapas:0];
+    
+genvar i;
+generate
+for(i=0; i<etapas; i=i+1) begin
+   inv_with_delay inv(aux_wire[i], aux_wire[i+1]);
+end
+endgenerate
+
+//assign aux_wire[0] = out & en;
+and andd(aux_wire[0], out, en);
+assign out = aux_wire[etapas];
+
+endmodule
+
+module USM_ringoscillator_nand4(
 
 	input en,
 	output out
@@ -31,7 +69,7 @@ wire aux_wire [etapas:0];
 genvar i;
 generate
 for(i=0; i<etapas; i=i+1) begin
-   inv_with_delay inv(aux_wire[i], aux_wire[i+1]);
+   nand_with_delay inv(aux_wire[i], aux_wire[i+1]);
 end
 endgenerate
 
--- src/top_tt03.v
+++ src/top_tt03.v
@@ -8,34 +8,28 @@ wire [7:0] rx_data;
 reg [7:0] tx_data;
 wire [1:0] send_sel;
 
-wire clk_external, clk_sel;
+wire clk_external, clk_sel, clk_internal;
 wire clk;
 
+wire osc_sel;
+
+//INPUTS
 assign clk_internal = io_in[0];
 assign clk_external = io_in[1];
 assign clk_sel = io_in[2];
-//assign clk = io_in[0];
 assign en = io_in[3];
 assign reset = io_in[4];
 assign rx = io_in[5];
+assign osc_sel = io_in[6];
 
+//OUTPUTS
 assign io_out[0] = tx;
-//assign io_out[1] = clk_external;
-//assign io_out[2] = clk_sel;
 assign io_out[7:1] = 0;
 
-wire out_osc;
+wire out_osc_inv, out_osc_nand, out_osc;
 wire [15:0] count;
 
 mux m(clk_external, clk_internal, clk_sel, clk);
-
-//always @* begin
-//	case(clk_sel)
-//		0: clk = clk_internal;
-//		1: clk = clk_external;
-//		default clk = clk_internal;
-//	endcase
-//end
 
 always @* begin
 	case(send_sel)
@@ -45,9 +39,11 @@ always @* begin
 	endcase
 end
 
-//test_ringosc_vargen osc(en, out_osc);
-//ring_osc osc(en, out_osc);
-USM_ringoscillator osc(en, out_osc);
+USM_ringoscillator_inv2 osc1(en, out_osc_inv);
+USM_ringoscillator_nand4 osc2(en, out_osc_nand);
+
+mux m2(out_osc_inv, out_osc_nand, osc_sel, out_osc);
+
 contador #(16) cont(out_osc, en, reset, clk, count);
 promedio #(16) prom(clk, reset, en, sum_en, count, promedio, sum_ready);
 
