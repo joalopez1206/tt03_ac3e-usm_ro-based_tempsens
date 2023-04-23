4209d7b62957377a4cdaeafdaf69f46869600184
lild4d4 <daniel.arevalos@sansano.usm.cl>
2023-04-23 06:01:42 +0200

    final_design

--- sim/tb_tt03.v
+++ sim/tb_tt03.v
@@ -3,23 +3,19 @@
 module tb_tt03(
 
     );
-    reg clk_sel;
-    reg clk_internal;
-    reg clk_external;
-    reg en;
-    reg reset;
-    reg rx;
-    reg osc_sel;
+    reg clk_sel, clk_internal, clk_external, en, reset, rx, osc_sel;
     wire tx;
+    reg en_inv_osc, en_nand_osc;
     wire [7:0] io_in, io_out;
     
     assign io_in[0] = clk_internal;
     assign io_in[1] = clk_external;
     assign io_in[2] = clk_sel;
-    assign io_in[3] = en;
-    assign io_in[4] = reset;
-    assign io_in[5] = rx;
-    assign io_in[6] = osc_sel;
+	assign io_in[3] = en_inv_osc;
+	assign io_in[4] = en_nand_osc;
+    assign io_in[5] = reset;
+    assign io_in[6] = rx;
+    assign io_in[7] = osc_sel;
     assign tx = io_out[0];
     
     top_tt03 DUT(io_in, io_out);
@@ -36,9 +32,10 @@ module tb_tt03(
     clk_external = 0;
     clk_sel = 1;
     osc_sel = 0;
-    en = 0;
+    en_inv_osc = 0;
+    en_nand_osc = 0;
     rx = 1;
-    #100000 en = 1;
+    #100000 en_inv_osc = 1;
     #15000 reset = 0;
     #52083;
     #1000000 rx = 0; //bit de inicio
@@ -53,6 +50,9 @@ module tb_tt03(
     #1000000 rx = 1; //bit de termino
     #100000000 clk_sel = 1; 
     osc_sel = 1;
+    #100000000 
+    en_inv_osc = 0;
+    en_nand_osc = 1;
     #100000000 $finish; 
     end
     
--- src/top_tt03.v
+++ src/top_tt03.v
@@ -4,11 +4,12 @@ module top_tt03(input [7:0] io_in, output [7:0] io_out);
 
 wire reset, rx, rx_ready, tx, tx_start, tx_busy;
 wire sum_ready, sum_en;
-wire osc_sel, en_inv_osc, en_nand_osc;
+wire osc_sel, en_inv_osc, en_nand_osc, en;
 wire clk_external, clk_sel, clk_internal, clk;
 wire out_osc_inv, out_osc_nand, out_osc;
 wire [23:0] promedio;
 wire [15:0] count;
+reg [15:0] count_reg;
 wire [7:0] rx_data;
 reg [7:0] tx_data;
 wire [1:0] send_sel;
@@ -18,23 +19,25 @@ assign clk_internal = io_in[0];
 assign clk_external = io_in[1];
 assign clk_sel = io_in[2];
 assign en_inv_osc = io_in[3];
-assign en_nand_osc = io_in[4]
+assign en_nand_osc = io_in[4];
 assign reset = io_in[5];
 assign rx = io_in[6];
 assign osc_sel = io_in[7];
 
 //OUTPUTS
 assign io_out[0] = tx;
-assign io_out[1] = promedio[8];
-assign io_out[2] = promedio[10];
-assign io_out[3] = promedio[13];
-assign io_out[4] = promedio[15];
-assign io_out[5] = promedio[17];
-assign io_out[6] = promedio[19];
-assign io_out[7] = promedio[21];
+assign io_out[1] = count_reg[8];
+assign io_out[2] = count_reg[9];
+assign io_out[3] = count_reg[10];
+assign io_out[4] = count_reg[11];
+assign io_out[5] = count_reg[12];
+assign io_out[6] = count_reg[13];
+assign io_out[7] = count_reg[14];
 
 //Clocks management
 mux m(clk_external, clk_internal, clk_sel, clk);
+
+mux m3(en_inv_osc, en_nand_osc, osc_sel, en);
 
 //tx_data management
 always @* begin
@@ -53,6 +56,12 @@ mux m2(out_osc_inv, out_osc_nand, osc_sel, out_osc);
 
 //Counters
 contador #(16) cont(out_osc, en, reset, clk, count);
+
+always @(posedge clk) begin
+	if(reset) count_reg <= 0;
+	else count_reg <= count; 
+end
+
 promedio #(24) prom(clk, reset, en, sum_en, count, promedio, sum_ready);
 
 //Controller
