1c0252cfe50c2432356b869bacf0212046d48ac2
lild4d4 <daniel.arevalos@sansano.usm.cl>
2023-04-24 16:56:39 +0200

    back_in_changes

--- src/contador.v
+++ src/contador.v
@@ -1,37 +1,37 @@
 module contador#(parameter N = 8)(input osc_clk, input en, input reset, input clk, output reg [N-1:0] count);
 
-//reg aux;
-//reg pre_aux;
-
-//always @(posedge osc_clk) begin
-//	if(reset) aux <= 1;
-//	//else aux <= pre_aux;
-//end
-
-//always @(posedge osc_clk) begin
-//	if(clk && aux) begin
-//		aux <= 0;
-//		count <= 0;
-//	end
-//	else if(en) begin
-//		if(!clk) aux<= 1;
-//		count <= count + 1;
-//	end
-//end
-
-reg clk_aux;
+reg aux;
+reg pre_aux;
 
 always @(posedge osc_clk) begin
-	if(reset) clk_aux <= 0;
-	else clk_aux <= clk;
+	if(reset) aux <= 1;
+	//else aux <= pre_aux;
 end
-
-wire clk_pulse;
-and andd(clk_pulse, clk, ~clk_aux);
 
 always @(posedge osc_clk) begin
-	if(reset | clk_pulse | !en) count <= 0;
-	else count <= count + 1;
+	if(clk && aux) begin
+		aux <= 0;
+		count <= 0;
+	end
+	else if(en) begin
+		if(!clk) aux<= 1;
+		count <= count + 1;
+	end
 end
+
+//reg clk_aux;
+
+//always @(posedge osc_clk) begin
+//	if(reset) clk_aux <= 0;
+//	else clk_aux <= clk;
+//end
+
+//wire clk_pulse;
+//and andd(clk_pulse, clk, ~clk_aux);
+
+//always @(posedge osc_clk) begin
+//	if(reset | clk_pulse | !en) count <= 0;
+//	else count <= count + 1;
+//end
 
 endmodule
